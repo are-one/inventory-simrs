54dddf7cb1c0779cd061cac0ac4adec62a2d2657
are-one <arwanpriantomangidi@gmail.com>
2025-12-31 11:16:54 +0800

    lembar kosong

--- resources/views/pemeriksaan/pdf-treadmill.blade.php
+++ resources/views/pemeriksaan/pdf-treadmill.blade.php
@@ -341,7 +341,7 @@
                 padding: 0 12mm 0 12mm;
                 margin: 0;
                 /* HAPUS page-break-after: always; */
-                page-break-after: always; /* Ganti dengan auto */
+                page-break-after: avoid; /* Ganti dengan auto */
                 page-break-inside: avoid;
             }
 
